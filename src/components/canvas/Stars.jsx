b41193d00e5732ac920e7c7f5a78c41fe2e63bda
Emily Dawson <lemilydawson@gmail.com>
2023-06-30 13:20:53 +0100

    [feature_29_add_env_file] added env file and replaced  tokens

--- src/components/canvas/Stars.jsx
+++ src/components/canvas/Stars.jsx
@@ -6,7 +6,7 @@ import * as random from "maath/random/dist/maath-random.esm";
 const Stars = (props) => {
   const ref = useRef();
   const [sphere] = useState(() =>
-    random.inSphere(new Float32Array(5000), { radius: 1.2 })
+    random.inSphere(new Float32Array(5001), { radius: 1.2 })
   );
 
   useFrame((state, delta) => {
@@ -19,7 +19,7 @@ const Stars = (props) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="white-100"
+          color="white"
           size={0.002}
           sizeAttenuation={true}
           depthWrite={false}
