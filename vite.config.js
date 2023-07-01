c58352abba8f43dee6016ccf1147430eada3277f
Emily Dawson <lemilydawson@gmail.com>
2023-07-01 18:48:28 +0100

    size warning changed to higher number

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-
 module.exports = {
   build: {
-    chunkSizeWarningLimit: 500, // Set your desired chunk size limit in kilobytes
+    chunkSizeWarningLimit: 2000000,
   },
 };
 // https://vitejs.dev/config/
