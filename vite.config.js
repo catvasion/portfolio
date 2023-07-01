1c17af9c33b3e76667be9d41fbc3c9ade094a060
Emily Dawson <lemilydawson@gmail.com>
2023-07-01 18:45:21 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+module.exports = {
+  build: {
+    chunkSizeWarningLimit: 500, // Set your desired chunk size limit in kilobytes
+  },
+};
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
