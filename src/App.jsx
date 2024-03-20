63ab2f3e0d95f47f73ac936da9cfe7289d511f27
catvasion <lemilydawson@gmail.com>
2024-03-20 22:40:57 +0000

    [C-55] add new experience info and tech icons to contants

--- src/App.jsx
+++ src/App.jsx
@@ -1,67 +1,63 @@
-import React, { useState, useEffect } from "react";
-import { BrowserRouter } from "react-router-dom";
-import { useProgress } from "@react-three/drei";
+import React, { useState, useEffect } from 'react'
+import { BrowserRouter } from 'react-router-dom'
+import { useProgress } from '@react-three/drei'
 
 import {
-  About,
-  Contact,
-  Experience,
-  Hero,
-  Navbar,
-  Tech,
-  Works,
-  StarsCanvas,
-  HeroLoader,
-} from "./components";
+	About,
+	Contact,
+	Experience,
+	Hero,
+	Navbar,
+	Tech,
+	Works,
+	StarsCanvas,
+	HeroLoader,
+} from './components'
 
 const App = () => {
-  const [isLoading, setIsLoading] = useState(true);
-  const { progress } = useProgress();
+	const [isLoading, setIsLoading] = useState(true)
+	const { progress } = useProgress()
 
-  useEffect(() => {
-    const loadData = async () => {
-      // Simulate loading the necessary data
-      // Replace this with your actual loading logic
-      await new Promise((resolve) => setTimeout(resolve, 1500));
+	useEffect(() => {
+		const loadData = async () => {
+			await new Promise((resolve) => setTimeout(resolve, 1500))
 
-      setIsLoading(false);
-    };
+			setIsLoading(false)
+		}
 
-    loadData();
-  }, []);
+		loadData()
+	}, [])
 
-  return (
-    <BrowserRouter>
-      <div className="bg-primary">
-        <Navbar />
-        {isLoading ? (
-          <div className="h-screen flex justify-center items-center">
-            <HeroLoader progress={progress} />
-          </div>
-        ) : (
-          <div>
-            {/* Render the content once isLoading is false */}
-            <div className="imageDiv min-h-[100vh] grayscale bg-filter"></div>
-            <div className="absolute top-[30%] bottom-[30%] right-0 left-0 h-fit">
-              <Hero />
-            </div>
-          </div>
-        )}
+	return (
+		<BrowserRouter>
+			<div className='bg-primary'>
+				<Navbar />
+				{isLoading ? (
+					<div className='h-screen flex justify-center items-center'>
+						<HeroLoader progress={progress} />
+					</div>
+				) : (
+					<div>
+						<div className='imageDiv min-h-[100vh] grayscale bg-filter'></div>
+						<div className='absolute top-[30%] bottom-[30%] right-0 left-0 h-fit'>
+							<Hero />
+						</div>
+					</div>
+				)}
 
-        <div className="">
-          {/* Render <Tech /> only on large screens */}
-          <Tech />
-        </div>
-        <Works />
-        <Experience />
-        <About />
-        <div className="relative z-0 bg-black-300">
-          <Contact />
-          <StarsCanvas />
-        </div>
-      </div>
-    </BrowserRouter>
-  );
-};
+				<div className=''>
+					<Tech />
+				</div>
+				<Works />
+				<Experience />
+				<About />
+				<div className='relative z-0 bg-black-300'>
+					<Contact />
+					<StarsCanvas />
+				</div>
+			</div>
+		</BrowserRouter>
+	)
+}
 
-export default App;
+export default App
